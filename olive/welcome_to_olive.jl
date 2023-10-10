e6f5e25c7f233f9ae9ba3b678abb7e2c5e10fa2d
emmy <52672675+emmaccode@users.noreply.github.com>
2023-10-10 17:26:14 -0600

    Add files via upload

---
+++ olive/welcome_to_olive.jl
@@ -0,0 +1,18 @@
+"""# welcome to olive!
+This is a simple example notebook to help get familiar with the `olive` editor! This will be a basic overview on the usage of olive's base. Olive's base consists of tools for Data Science and software development in the julia programming language.
+
+"""
+#==|||==#
+using Toolips
+# this is a code cell, we write Julia code into it.
+println("hello world!")
+
+#==output[code]
+hello world!<br>
+==#
+#==|||==#
+img("olivenbs", src = "https://github.com/ChifiSource/image_dump/blob/main/olive/olivenbs.png?raw=true", width = 350)
+#==output[code]
+"<img id=olivenbs src=https://github.com/ChifiSource/image_dump/blob/main/olive/olivenbs.png?raw=true width=350></img>"
+==#
+#==|||==#
